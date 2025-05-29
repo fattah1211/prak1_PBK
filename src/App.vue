2ac81091d00b68fde44f466ad6d6b010ee0d2263
fattah1211 <fattahfitriansyah11@gmail.com>
2025-05-29 11:39:20 +0700

    tambah foto dan ubah data diri

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,13 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="profile">
+    <img src="./assets/saya.jpg" alt="Foto Profil" class="profile-pic" />
+    <h1>Fattah Fitriansyah</h1>
+    <p>Saya merupakan mahasiswa prodi teknik informatika semester 4</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
+
 
 <style scoped>
 .logo {
@@ -21,10 +18,21 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+.profile {
+  text-align: center;
+  padding: 2rem;
+}
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+.profile-pic {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-bottom: 1rem;
 }
 </style>
